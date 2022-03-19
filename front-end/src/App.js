09360a77f5bb3b6ade771e37777e189a762aaf36
Max <maxgt734@gmail.com>
2022-03-19 15:55:49 -0600

    Se instalaron dependencias necesarias.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Home from "./views/Home";
+
 
 function App() {
+  useEffect(() => {
+    if(window.ethereum){
+      window.ethereum.request({
+        method: "eth_requestAccounts",
+      }).then(accounts => console.log(accounts));
+    }
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" exact element={<Home/>} />
+    </Routes>
   );
 }
 
---
+++ front-end/src/views/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+      <>
+        <p>Hello World</p>
+      </>
+    );
+  }
+  
+export default Home;
